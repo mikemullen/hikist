29b296dd1620ab1b21d7d6492096dbe9620feb98
Mike Mullen <m@m-mullen.com>
2013-05-02 11:12:15 -0700

    Initial commit with postgresQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hikist::Application.config.secret_token = 'd684d960c432f0fbb09f3086cda3da1647b5bbdc15dac0823326682992309ff21543db793113f689f9be171cbeb196bc24da2dbc5d4da6f837b45793571cfb51'
