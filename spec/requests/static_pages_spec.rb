2bb2cf82211d935ca171e0e86ed78690060922a3
Mike Mullen <m@m-mullen.com>
2013-05-08 09:15:38 -0700

    Make a basic User model (including secure password)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,25 +4,43 @@ describe "Static Pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+  	it { should have_selector('h1',    text: heading) }
+  	it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
   	before { visit root_path }
+  	let(:heading)    { 'Welcome to Hikist' }
+  	let(:page_title) { '' }
 
-  	it { should have_selector('h1', text: 'Welcome to Hikist') }
-  	it { should have_selector('title', text: full_title('')) }
+	it_should_behave_like "all static pages"
   	it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
   	before { visit help_path }
+  	let(:heading)    { 'Help' }
+  	let(:page_title) { 'Help' }
 
-  	it { should have_selector('h1', text: "Help") }
-   	it { should have_selector('title', text: full_title('Help')) }
+  	it_should_behave_like "all static pages"
   end
 
   describe "About page" do
   	before { visit about_path }
+  	let(:heading)    { 'About' }
+  	let(:page_title) { 'About' }
 
-  	it { should have_selector('h1', text: "About") }
-  	it { should have_selector('title', text: full_title('About')) }
+  	it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+  	visit root_path
+  	click_link "About"
+  	page.should have_selector('title', text: full_title('About'))
+  	click_link "Help"
+  	page.should have_selector('title', text: full_title('Help'))
+  	click_link "Hikist"
+  	page.should have_selector('title', text: full_title(''))
   end
 end
