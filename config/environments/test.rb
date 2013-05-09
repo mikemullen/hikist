0f6dfb25d0f610c86cd0c600e32c00e670c4fc00
Mike Mullen <m@m-mullen.com>
2013-05-08 19:38:15 -0700

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ Hikist::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up testing by lowering BCrypts cost.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
